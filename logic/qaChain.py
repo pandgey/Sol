b28fcde78835258c31d7a8ab9db4f012002edc05
pandgey <krittapong.ti@gmail.com>
2025-10-25 21:06:44 +1100

    revamp the whole algorithm (again...)

--- logic/qaChain.py
+++ logic/qaChain.py
@@ -1,6 +1,6 @@
 from langchain_community.llms import HuggingFacePipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 
